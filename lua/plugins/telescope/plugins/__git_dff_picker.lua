ce4cd33c6b59864282074eb318bc7f88a2958b51
Alexandre Sage <alexandre.sage@adsoftware.fr>
2024-06-26 12:07:24 +0000

    feat: api

--- lua/plugins/telescope/plugins/__git_dff_picker.lua
+++ lua/plugins/telescope/plugins/__git_dff_picker.lua
@@ -1,40 +1,14 @@
+local api = require("plugins.telescope.plugins.api")
 local user_cmd = require("helpers").create_user_command
 local map_key = require("helpers").map_key
 local cmd = require("helpers").normal_cmd
-local log = require("helpers").log_to_file
-local actions = require("telescope.actions")
-local action_state = require("telescope.actions.state")
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
-local conf = require("telescope.config").values
-local git = require("gitsigns")
-local function is_current_line_in_git_diff(bufnr)
-  -- Get the current buffer and line number
-  local lnum = vim.api.nvim_win_get_cursor(0)[1]
-
-  -- Get the hunks for the current buffer
-  local hunks = require("gitsigns").get_hunks(bufnr)
-  if not hunks then
-    return false
-  end
-
-  -- Check if the current line is part of any hunk
-  for _, hunk in ipairs(hunks) do
-    if lnum >= hunk.added.start and lnum <= hunk.added.start + hunk.added.count - 1 then
-      return { hunk.added.start, hunk.added.start + hunk.added.count - 1 }
-    end
-    if lnum >= hunk.removed.start and lnum <= hunk.removed.start + hunk.removed.count - 1 then
-      return { hunk.removed.start, hunk.removed.start + hunk.removed.count - 1 }
-    end
-  end
-
-  return false
-end
 
 local function get_opts(diff)
   if diff then
     return {
-      { "Revert hunk", 1 },
+      { "=> Revert hunk from: L-" .. diff[1] .. " to L-" .. diff[2], 1 },
       { "Stage hunk", 2 },
       { "Next hunk", 3 },
       { "Previous hunk", 4 },
@@ -49,47 +23,22 @@ end
 
 local git_actions = function(opts)
   opts = opts or {}
-  local bufnr = vim.api.nvim_get_current_buf()
-  local diff = is_current_line_in_git_diff(bufnr)
+  local diff = api.get_diff()
   pickers
     .new(opts, {
       prompt_title = "Git actions",
       finder = finders.new_table({
         results = get_opts(diff),
-        entry_maker = function(entry)
-          log("/home/adsoftware/logs/ga.log", entry[1])
-          return {
-            value = entry[2],
-            display = entry[1],
-            ordinal = entry[3] or entry[2],
-            lnum = entry[3] or entry[2],
-          }
-        end,
+        entry_maker = api.make_entry,
       }),
       -- sorter = conf.generic_sorter(opts),
-      attach_mappings = function(prompt_bufnr, map)
-        actions.select_default:replace(function()
-          actions.close(prompt_bufnr)
-          local selection = action_state.get_selected_entry()
-          print(selection)
-          if selection.value == 1 then
-            git.reset_hunk(diff)
-          elseif selection.value == 2 then
-            git.stage_hunk(diff)
-          elseif selection.value == 3 then
-            git.nav_hunk("next")
-          elseif selection.value == 4 then
-            git.nav_hunk("prev")
-          end
-        end)
-        return true
-      end,
+      attach_mappings = api.attach_mapping(diff),
     })
     :find()
 end
 
 user_cmd("GitActions", function()
-  git_actions(require("telescope.themes").get_dropdown({}))
+  git_actions(require("telescope.themes").get_cursor({ layout_config = { anchor = "SE" } }))
 end, "?")
 
 map_key({ "v", "n" }, "<leader>Ga", cmd("GitActions"), { desc = "Toggle git actions for underline hunk or buffer" })
---
+++ lua/plugins/telescope/plugins/api.lua
@@ -0,0 +1,75 @@
+local git = require("gitsigns")
+local action_state = require("telescope.actions.state")
+local actions = require("telescope.actions")
+local helpers = require("helpers")
+
+local function highlight_text(bufnr, start_line, end_line)
+  local ns_id = vim.api.nvim_create_namespace("GitActions")
+
+  for line = start_line, end_line do
+    vim.api.nvim_buf_add_highlight(bufnr, 0, "", line, 0, -1)
+  end
+end
+
+local function is_current_line_in_git_diff(bufnr)
+  local lnum = vim.api.nvim_win_get_cursor(0)[1]
+  local hunks = git.get_hunks(bufnr)
+
+  if not hunks then
+    return false
+  end
+
+  for _, hunk in ipairs(hunks) do
+    if lnum >= hunk.added.start and lnum <= hunk.added.start + hunk.added.count - 1 then
+      return { hunk.added.start, hunk.added.start + hunk.added.count - 1 }
+    end
+    if lnum >= hunk.removed.start and lnum <= hunk.removed.start + hunk.removed.count - 1 then
+      return { hunk.removed.start, hunk.removed.start + hunk.removed.count - 1 }
+    end
+  end
+
+  return false
+end
+
+local function make_entry(entry)
+  return {
+    value = entry[2],
+    display = entry[1],
+    ordinal = entry[3] or entry[2],
+    lnum = entry[3] or entry[2],
+  }
+end
+
+local function attach_mapping(diff)
+  return function(prompt_bufnr, map)
+    actions.select_default:replace(function()
+      actions.close(prompt_bufnr)
+      local selection = action_state.get_selected_entry()
+      print(selection)
+      if selection.value == 1 then
+        git.reset_hunk(diff)
+      elseif selection.value == 2 then
+        git.stage_hunk(diff)
+      elseif selection.value == 3 then
+        git.nav_hunk("next")
+      elseif selection.value == 4 then
+        git.nav_hunk("prev")
+      end
+    end)
+    return true
+  end
+end
+local function get_diff()
+  local bufnr = vim.api.nvim_get_current_buf()
+  local is_diff = is_current_line_in_git_diff(bufnr)
+  if is_diff then
+    highlight_text(bufnr, is_diff[1], is_diff[2])
+  end
+  return is_diff
+end
+return {
+  make_entry = make_entry,
+  attach_mapping = attach_mapping,
+  get_diff = get_diff,
+  is_current_line_in_git_diff = is_current_line_in_git_diff,
+}
