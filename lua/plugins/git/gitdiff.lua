d99dc2162e65eb0f7ac8435ab1f73eebf111b002
Alexandre Sage <alexandre.sage@adsoftware.fr>
2024-07-04 15:28:35 +0000

    feat: oil

--- lua/plugins/git/gitdiff.lua
+++ lua/plugins/git/gitdiff.lua
@@ -1,3 +1,3 @@
 local Plug = { "sindrets/diffview.nvim" }
-Plug.dependencies = { "nvim-lua/plenary.nvim" } -- required
+-- Plug.dependencies = { "nvim-lua/plenary.nvim" } -- required
 return Plug
---
+++ lua/plugins/utils/oil.lua
@@ -0,0 +1,7 @@
+local Plug = { "stevearc/oil.nvim" }
+
+Plug.dependencies = { "nvim-tree/nvim-web-devicons" }
+
+Plug.opts = {}
+
+return Plug
