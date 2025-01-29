c9d9e1e0304a7c5167173b720306e9b4a1b3eef6
Alexandre Sage (ASE) <alexandre.sage@adsoftware.fr>
2025-01-29 12:55:09 +0000

    feat: http repo

--- lua/plugins/utils/markdownsettings.lua
+++ lua/plugins/utils/markdownsettings.lua
@@ -1,9 +1,11 @@
-local Plug = { "toppair/peek.nvim", lazy = true }
+local Plug = { "toppair/peek.nvim" }
 
 Plug.event = "VeryLazy"
 Plug.build = "deno task --quiet build:fast"
 Plug.config = function()
-  require("peek").setup()
+  require("peek").setup({
+    app = { "chromium", "--new-window" },
+  })
   vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
   vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
 end
