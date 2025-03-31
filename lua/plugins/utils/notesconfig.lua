8468a70c899aba92a2a41a1b5e3b18de63d7a178
Alexandre Sage <alexandre.sage@adsoftware.fr>
2025-03-31 12:54:27 +0000

    fix: notes

--- lua/plugins/utils/notesconfig.lua
+++ lua/plugins/utils/notesconfig.lua
@@ -4,6 +4,18 @@ Plug.opts = {
     "~",
   },
 }
+Plug.keys = {
+  -- You can use telescope to search the current note space:
+  {
+    "<leader>tn", -- [t]elescope [n]ote
+    function()
+      require("telescope.builtin").live_grep({
+        cwd = require("note.api").current_note_root(),
+      })
+    end,
+    mode = "n",
+  },
+}
 
 Plug.cmd = "Note"
 Plug.ft = "note"
